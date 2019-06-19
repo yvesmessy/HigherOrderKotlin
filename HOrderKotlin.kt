6c4595e6388628409b9f4858443ef111a40aa2ee
Yves Guillaume A Messy <yvesmessy@users.noreply.github.com>
2019-06-19 17:16:33 -0400

    updated to include integer and string concatenation in lambda

--- HOrderKotlin.kt
+++ HOrderKotlin.kt
@@ -9,14 +9,26 @@ fun main(args: Array<String>) {
 val yvesLambdaFunc: (Int, Int ) -> Int = { x , y -> x + y }
 
  addition(8, 9, yvesLambdaFunc)
+ 
+
+val yvesNameAgePrint: (String, Int ) -> String = { a , b -> a + b}  
+
+ namedperson("Stewart", 22, yvesNameAgePrint)
 
 }
-
 
 fun addition(a: Int, b: Int, yvesLambdaFunc:(Int, Int) -> Int ) {
     
     var result = yvesLambdaFunc(a , b)
     
-    print(result)
+    println(result)
+    
+}
+
+fun namedperson(a: String, b: Int, yvesNameAgePrint:(String, Int) -> String ) {
+    
+    var person = yvesNameAgePrint(a, b)
+    
+    println(person)
     
 }
